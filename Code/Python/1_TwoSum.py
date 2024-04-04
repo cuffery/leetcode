d5063d53f6bea6ab25784eb3916a5583b34ebe71
cuffery <cuffery@hotmail.com>
2024-04-03 21:15:30 -0700

    adding env file for conda

--- Code/Python/1_TwoSum.py
+++ Code/Python/1_TwoSum.py
@@ -1,6 +1,20 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum_bruteForce(self, nums: List[int], target: int) -> List[int]:
+        ## this is O(n^2) as we have double loops
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if(i != j and nums[i]+nums[j] == target): 
+            for j in range(i+1, len(nums)): ### simple tweak
+                if(nums[i]+nums[j] == target): 
                     return [i,j]
+    
+    def twoSum_hashTable(self, nums: List[int], target: int) -> List[int]:
+        ## in this approach we trade off space for time
+        ## O(1) look up time; 
+        ## overall time complexity is O(n) as we need to build hashmap (which is a dict)
+        hashmap = {}
+        for i in range(len(nums)):
+            ## build hashmap
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
---
+++ Notes/python_syntax.md
@@ -0,0 +1,12 @@
+# Python Syntax Notes
+...notes on python default functions, syntax that I have to look up. Keeping them in one place for ease of reference
+
+## Functions
+- `for loop`: `for i in range(10)` loops over `0` to `9`
+- `IF`: `if...and...`
+
+## Data Structures
+- list v. dict: 
+    - list is created using `[]` while dict by `{}`; 
+    - list can do **list comprehension**
+- hash table: can be built using a dict (see [Two Sum](../Code/Python/1_TwoSum.py))
